c981aae3acac4b6ad6ab9a92e9fbfe52b59075d3
Thamme Gowda <tg@isi.edu>
2018-06-24 18:08:39 -0700

    An EOS token aware beam decoder

--- tgnmt/module/decoder.py
+++ tgnmt/module/decoder.py
@@ -1,6 +1,6 @@
 import torch
 from tgnmt import log, device, my_tensor as tensor
-from tgnmt.dataprep import BLANK_TOK, BOS_TOK, subsequent_mask
+from tgnmt.dataprep import BLANK_TOK, BOS_TOK, EOS_TOK, subsequent_mask
 from tgnmt.module.t2t import EncoderDecoder
 
 
@@ -8,6 +8,7 @@ class Decoder:
 
     pad_val = BLANK_TOK[1]
     bos_val = BOS_TOK[1]
+    eos_val = EOS_TOK[1]
     default_beam_size = 5
 
     def __init__(self, model, exp):
@@ -35,84 +36,159 @@ class Decoder:
         memory = self.model.encode(x_seqs, x_mask)
         batch_size = x_seqs.size(0)
         ys = torch.full(size=(batch_size, 1), fill_value=self.bos_val, dtype=torch.long, device=device)
+        scores = torch.zeros(batch_size, device=device)
+
+        actives = ys[:, -1] != self.eos_val
         for i in range(max_len):
+            if actives.sum() == 0:  # all sequences Ended
+                break
             out = self.model.decode(memory, x_mask, ys, subsequent_mask(ys.size(1)))
-            prob = self.model.generator(out[:, -1])
-            _, next_word = torch.max(prob, dim=1)
+            log_prob = self.model.generator(out[:, -1])
+            max_prob, next_word = torch.max(log_prob, dim=1)
+            scores += max_prob
             ys = torch.cat([ys, next_word.view(batch_size, 1)], dim=1)
-        return ys[:, 1:]  # exclude BOS
+            actives &= ys[:, -1] != self.eos_val
 
-    def init_beam(self, x_seqs, beam_size):
+        result = []
+        for i in range(batch_size):
+            result.append((scores[i].item(), ys[i, 1:]))
+        return result
 
-        x_mask = (x_seqs != self.pad_val).unsqueeze(1)
-        memory = self.model.encode(x_seqs, x_mask)
-        batch_size = x_seqs.size(0)
-        assert batch_size == 1, 'Currently doing one sequence at a time. TODO: do full batch'
+    @staticmethod
+    def masked_select(tensor, mask):
+        assert tensor.shape[0] == mask.shape[0]
+        assert mask.shape[1] == 1
+        selected = tensor.masked_select(mask)
+        return selected.view(-1, tensor.size(1))
 
-        # Initialize k beams, with BOS token
-        ys = torch.full(size=(batch_size, 1), fill_value=self.bos_val, dtype=torch.long, device=device)
-        out = self.model.decode(memory, x_mask, ys, subsequent_mask(ys.size(1)))
-        prob = self.model.generator(out[:, -1])  # [batch_size, Vocab]
-
-        top_probs, nxt_idx = torch.topk(prob, k=beam_size, dim=1)  # [batch, beam_size],[batch, beam_size]
-
-        # repeat evrything beam_size times
-        expanded_ys = ys.repeat(1, beam_size).view(batch_size * beam_size, -1)
-        beamed_ys = torch.cat([expanded_ys, nxt_idx.view(-1, 1)], dim=1)
-        beam_scores = top_probs.log().view(batch_size*beam_size, 1)
-        memory_size = memory.size()
-        beamed_mem = memory.repeat(1, beam_size, 1).view(memory_size[0]*beam_size, memory_size[1], memory_size[2])
-        mask_size = x_mask.size()
-        beamed_mask = x_mask.repeat(1, beam_size, 1).view(mask_size[0]*beam_size, mask_size[1], mask_size[2])
-        return beamed_mem, beamed_mask, beamed_ys, beam_scores
-
-    def beam_decode(self, x_seqs, max_len, beam_size=default_beam_size, **args):
+    def beam_decode(self, x_seqs, max_len, beam_size=default_beam_size, num_hyp=None, **args):
         """Implements beam decoding"""
+        # repeat beam size
+        batch_size = x_seqs.size(0)
+        assert batch_size == 1  # TODO: test large batches
+        if not num_hyp:
+            num_hyp = beam_size
 
-        memory, x_mask, ys, scores = self.init_beam(x_seqs, beam_size)
+        # Everything beamed_*  below is the batch repeated beam_size times
+        beamed_batch_size = batch_size * beam_size
+        beamed_x_seqs = x_seqs.repeat(1, beam_size).view(beamed_batch_size, -1)
+        beamed_x_mask = (beamed_x_seqs != self.pad_val).unsqueeze(1)
+        beamed_memory = self.model.encode(beamed_x_seqs, beamed_x_mask)
+
+        beamed_ys = torch.full(size=(beamed_batch_size, 1), fill_value=self.bos_val, dtype=torch.long, device=device)
+        beamed_scores = torch.zeros((beamed_batch_size, 1), device=device)
+
+        beam_active = torch.ones((beamed_batch_size, 1), dtype=torch.uint8, device=device)
+        # zeros means ended, one means active
         for t in range(1, max_len):
-            out = self.model.decode(memory, x_mask, ys, subsequent_mask(ys.size(1)))
-            prob = self.model.generator(out[:, -1])  # [batch*beam, Vocab]
-            # broad cast scores along row and sum  log probabilities
-            next_scores = scores.view(-1, 1) + prob.log()
+            if beam_active.sum() == 0:
+                break
+            out = self.model.decode(beamed_memory, beamed_x_mask, beamed_ys, subsequent_mask(beamed_ys.size(1)))
+            log_prob = self.model.generator(out[:, -1])  # [batch*beam, Vocab]
+            # broad cast scores along row (broadcast) and sum  log probabilities
 
-            top_scores, nxt_idx = torch.topk(next_scores, k=beam_size, dim=1)  # [batch*beam, beam],[batch*beam, beam]
+            next_scores = beamed_scores + beam_active.float() * log_prob  # Zero out inactive beams
+
+            top_scores, nxt_idx = next_scores.topk(k=beam_size, dim=-1)  # [batch*beam, beam],[batch*beam, beam]
             # Now we got beam_size*beam_size heads, task: shrink it to beam_size
             # Since the ys will change, after re-scoring, we will make a new tensor for new ys
-            new_ys = torch.full(size=(ys.size(0), ys.size(1) + 1), fill_value=self.pad_val, device=device,
-                                dtype=torch.long)
-            for i in range(x_seqs.size(0)):
-                # beams of i'th sequence in batch
-                start, end = i*beam_size, (i+1) * beam_size
-                seqi_scores, seqi_nxt_idx = top_scores[start:end, :], nxt_idx[start:end, :]
+            new_ys = torch.full(size=(beamed_batch_size, beamed_ys.size(1) + 1), fill_value=self.pad_val,
+                                device=device, dtype=torch.long)
 
-                # picking top k out of k*k
-                seqi_top_scores, seqi_nxt_idx_idx = seqi_scores.view(-1).topk(k=beam_size)
-                # copy the old beams corresponding to current top scores
-                scores[start:end, 0] = seqi_top_scores
-                beam_idx = seqi_nxt_idx_idx / beam_size
-                new_ys[start:end, :-1] = ys[start:end, :].index_select(0, beam_idx)
+            for i in range(batch_size):
+                # going to picking top k out of k*k beams for each sequence in batch
+                # beams of i'th sequence in batch have this start and end
+                start, end = i * beam_size, (i+1) * beam_size
+                if beam_active[start:end].sum() == 0:
+                    # current sequence ended
+                    new_ys[:start:end, :-1] = beamed_ys[start:end]
+                    continue
+
+                if t == 1:
+                    # initialization ; since sequences are duplicated in the start, we just pick the first row
+                    # it is must, otherwise, beam will select the top 1 of each beam which is same but duplicated
+                    seqi_top_scores, seqi_nxt_ys = top_scores[start, :], nxt_idx[start, :]
+                    # ys seen so far remains same; no reordering
+                    new_ys[start:end, :-1] = beamed_ys[start:end]
+                    seqi_top_scores = seqi_top_scores.view(-1, 1)
+                else:
+                    seqi_nxt_ys = torch.full((beam_size,1), fill_value=self.pad_val, device=device)
+                    seqi_top_scores = torch.zeros((beam_size, 1), device=device)
+
+                    # ignore the inactive beams, don't grow them any further
+                    # INACTIVE BEAMS: Preserve the inactive beams, just copy them
+                    seqi_inactive_mask = (beam_active[start:end, -1] == 0).view(-1, 1)
+                    seqi_inactive_count = seqi_inactive_mask.sum()
+                    active_start = start + seqi_inactive_count    # [start, ... active_start-1, active_start, ... end]
+                    if seqi_inactive_count > 0: # if there are some inactive beams
+                        seqi_inactive_ys = self.masked_select(beamed_ys[start:end, :], seqi_inactive_mask)
+                        new_ys[start: active_start, :-1] = seqi_inactive_ys  # Copy inactive beams
+                        seqi_top_scores[start:active_start, :] = \
+                            self.masked_select(beamed_scores[start:end, :], seqi_inactive_mask)
+
+                    # ACTIVE BEAMS: the remaining beams should be let to grow
+                    seqi_active_mask = (beam_active[start:end, -1] == 1).view(-1, 1)
+                    seqi_active_count = seqi_active_mask.sum()  # task is to select these many top scoring beams
+
+                    seqi_scores = self.masked_select(top_scores[start:end, :], seqi_active_mask)
+                    seqi_nxt_idx = self.masked_select(nxt_idx[start:end, :], seqi_active_mask)
+
+                    seqi_active_top_scores, seqi_nxt_idx_idx = seqi_scores.view(-1).topk(k=seqi_active_count)
+                    seqi_top_scores[active_start:end, 0] = seqi_active_top_scores
+                    seqi_nxt_ys[active_start: end, 0] = seqi_nxt_idx.view(-1)[seqi_nxt_idx_idx]
+
+                    # Select active ys
+                    active_beam_idx = seqi_nxt_idx_idx / beam_size
+                    seqi_active_ys = self.masked_select(beamed_ys[start:end, :], seqi_active_mask)
+                    seqi_active_old_ys = seqi_active_ys.index_select(0, active_beam_idx)
+                    new_ys[active_start:end, :-1] = seqi_active_old_ys
+
+                    # Update status of beam_active flags
+                    beam_active[active_start:end, :] = self.masked_select(beam_active[start:end, :], seqi_active_mask) \
+                        .index_select(0, active_beam_idx)
+                    if active_start > start:
+                        beam_active[start:active_start, -1] = 0  # inactive beams are set to zero
+
+                beamed_scores[start:end, :] = seqi_top_scores
                 # copy the new word indices to last step
-                new_ys[start:end, -1] = seqi_nxt_idx.view(-1)[seqi_nxt_idx_idx]
+                new_ys[start:end, -1] = seqi_nxt_ys.view(-1)
+            beamed_ys = new_ys
+            # AND to update active flag
+            beam_active = beam_active & (beamed_ys[:, -1] != self.eos_val).view(beamed_batch_size, 1)
 
-            ys = new_ys
-        # TODO: Special treatment for EOS tokens and padding tokens in scoring
-        return ys[:, 1:], scores  # exclude BOS
+        result = []
+        # reverse sort based on the score
+        for i in range(batch_size):
+            result.append([])
+            start, end = i * beam_size, (i + 1) * beam_size
+            scores, indices = beamed_scores[start:end].sort()
+            for j in range(beam_size):
+                if len(result[-1]) == num_hyp:
+                    continue
+                result[-1].append((scores[j].item(), beamed_ys[start+indices[j], 1:].squeeze()))
+        return result
 
-    def decode_sentence(self, line: str, max_len=20, **args) -> str:
+    def decode_sentence(self, line: str, max_len=20, prepared=False, **args) -> str:
         in_toks = line.strip().split()
-        in_seq = self.exp.src_field.seq2idx(in_toks, add_bos=True, add_eos=True)
+        if prepared:
+            in_seq = [int(t) for t in in_toks]
+            if in_seq[0] != self.bos_val:
+                in_seq.insert(0, self.bos_val)
+            if in_seq[-1] != self.eos_val:
+                in_seq.append(self.eos_val)
+        else:
+            in_seq = self.exp.src_field.seq2idx(in_toks, add_bos=True, add_eos=True)
         in_seqs = tensor(in_seq, dtype=torch.long).view(1, -1)
 
-        greedy_out = self.greedy_decode(in_seqs, max_len, **args)[0]
+        greedy_score, greedy_out = self.greedy_decode(in_seqs, max_len, **args)[0]
         greedy_toks = self.exp.tgt_field.idx2seq(greedy_out, trunc_eos=True)
         greedy_out = ' '.join(greedy_toks)
-        log.info(f'Greedy : {greedy_out}')
+        log.info(f'Greedy : score: {greedy_score} :: {greedy_out}')
 
-        beam_outs, scores = self.beam_decode(in_seqs, max_len, **args)
-        for i in range(beam_outs.size(0)):
-            out = ' '.join(self.exp.tgt_field.idx2seq(beam_outs[i], trunc_eos=True))
-            log.info(f"Beam {i}: score:{scores[i]} :: {out}")
+        result = self.beam_decode(in_seqs, max_len, **args)[0]
+        for i, (score, beam_toks) in enumerate(result):
+            out = ' '.join(self.exp.tgt_field.idx2seq(beam_toks, trunc_eos=True))
+            log.info(f"Beam {i}: score:{score} :: {out}")
         return greedy_out
 
     def decode_file(self, inp, out, **args):
@@ -130,4 +206,3 @@ class Decoder:
             out_line = self.decode_sentence(input, **args)
             out.write(f'{out_line}\n')
             log.info(f"OUT: {i}: {out_line}\n")
-
