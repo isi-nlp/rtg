34c2dd414c9d0c97b840ae6d1de51c579cc412b5
Thamme Gowda <tg@isi.edu>
2018-07-01 16:55:12 -0700

    Calculate size of tensors based on datatype

--- tgnmt/utils.py
+++ tgnmt/utils.py
@@ -5,6 +5,21 @@ from functools import reduce
 import operator as op
 
 
+# Size of each element in tensor
+tensor_size = {
+    'torch.Tensor': 4,
+    'torch.FloatTensor': 4,
+    'torch.DoubleTensor': 8,
+    'torch.HalfTensor': 2,
+    'torch.ByteTensor': 1,
+    'torch.CharTensor': 1,
+    'torch.ShortTensor': 2,
+    'torch.IntTensor': 4,
+    'torch.LongTensor': 8
+}
+tensor_size.update({t.replace('torch.', 'torch.cuda.'): size for t, size in tensor_size.items()})
+
+
 def log_tensor_sizes(writer=log.info, min_size=1024):
     """
     Forces garbage collector and logs all the current tensors
@@ -12,15 +27,25 @@ def log_tensor_sizes(writer=log.info, min_size=1024):
     """
     log.info("Collecting tensor allocations")
     gc.collect()
-    tensors = (obj for obj in gc.get_objects()
-               if torch.is_tensor(obj)
-               # or (hasattr(obj, 'data') and torch.is_tensor(obj.data))
-               )
+
+    def is_tensor(obj):
+        if torch.is_tensor(obj):
+            return True
+        try:    # some native objects raise exceptions
+            return hasattr(obj, 'data') and torch.is_tensor(obj.data)
+        except:
+            return False
+
+    tensors = filter(is_tensor, gc.get_objects())
     stats = ((reduce(op.mul, obj.size()) if len(obj.size()) > 0 else 0,
               obj.type(), tuple(obj.size()), hex(id(obj))) for obj in tensors)
+    stats = ((n*tensor_size[typ], n, typ, *blah) for n, typ, *blah in stats)
     stats = (x for x in stats if x[0] > min_size)
     sorted_stats = sorted(stats, key=lambda x: x[0])
-    lines = (f'{i:4}\t{size:12,}\t{typ}\t{shape}\t{_id}' for i, (size, typ, shape, _id) in enumerate(sorted_stats))
+
+    writer("####\tApprox Bytes\tItems       \tShape   \tObject ID")
+    lines = (f'{i:4}\t{size:12,}\t{n:12,}\t{typ}\t{shape}\t{_id}'
+             for i, (size, n, typ, shape, _id) in enumerate(sorted_stats))
     log.info("==== Tensors and memories === ")
     for i, l in enumerate(lines):
         writer(l)
