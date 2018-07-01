9ef5f2565d6198f58b3da02c17be22da11860e49
Thamme Gowda <tg@isi.edu>
2018-07-01 16:58:27 -0700

    support for memory profiler (NMT_PROFILER=memory to activate it)

--- tgnmt/__init__.py
+++ tgnmt/__init__.py
@@ -8,12 +8,31 @@ import torch
 device_name = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 device = torch.device(device_name)
 log.debug(f'device: {device}')
+profiler = None
+if os.environ.get('NMT_PROFILER') == 'memory':
+    import memory_profiler
+    profiler = memory_profiler.profile
+    log.info('Setting memory profiler')
 
 
 def my_tensor(*args, **kwargs):
     return torch.tensor(*args, device=device, **kwargs)
 
 
+def profile(func, *args):
+    """
+    :param func: function to profile
+    :param args: any addtional args for profiler
+    :return:
+    """
+
+    if not profiler:
+        return func
+    return profiler(func, *args)
+
+
 from .dataprep import Field, BatchIterable, Batch
 from tgnmt.exp import TranslationExperiment
 from tgnmt.module import seq2seq
+
+
