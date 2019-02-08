4331e2149377b1db6b571e66d3da0328fdf7cc2c
Thamme Gowda <tg@isi.edu>
2019-02-07 17:47:46 -0800

    baseclasses Model -> Language Model and Translation Model

--- rtg/module/__init__.py
+++ rtg/module/__init__.py
@@ -1,15 +1,33 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, ABC
 import torch.nn as nn
 
 
-class NMTModel(nn.Module):
-    """"
-    base class for all Sequence to sequence (NMT) models
-    """
-    # TODO: move stuff here that is common to all
-    pass
+class Model(nn.Module):
 
     @property
     @abstractmethod
     def model_dim(self):
         pass
+
+    @property
+    @abstractmethod
+    def model_type(self):
+        pass
+
+    @property
+    @abstractmethod
+    def vocab_size(self):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def make_model(cls, *args, **kwargs):
+        raise NotImplementedError
+
+
+class NMTModel(Model, metaclass=ABCMeta):
+    """"
+    base class for all Sequence to sequence (NMT) models
+    """
+    # TODO: move stuff here that is common to all
+    pass
