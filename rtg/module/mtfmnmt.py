b77537c2aa303b804a2e1008bd700bb73770beb7
Thamme Gowda <tg@isi.edu>
2019-05-13 14:07:08 -0700

    mtfm : restore sentence repr concat at the embedding functionality

--- rtg/module/mtfmnmt.py
+++ rtg/module/mtfmnmt.py
@@ -6,6 +6,7 @@
 
 import inspect
 import copy
+import math
 import torch
 import torch.nn as nn
 
@@ -13,7 +14,34 @@ from rtg.module.tfmnmt import (Encoder, EncoderLayer, PositionwiseFeedForward, P
                                Generator, MultiHeadedAttention, Embeddings, TransformerNMT,
                                TransformerTrainer, SublayerConnection, LayerNorm, clones)
 from rtg import TranslationExperiment as Experiment, log
-from rtg.dataprep import CLS_TOK_IDX as cls_idx
+from rtg.dataprep import CLS_TOK_IDX as cls_idx, PAD_TOK_IDX as pad_idx
+
+
+class MEmbeddings(nn.Module):
+
+    def __init__(self, d_model, vocab):
+        super().__init__()
+        self.lut = nn.Embedding(vocab, d_model, padding_idx=pad_idx)
+        self.vocab = vocab
+        self.d_model = d_model
+        # merge [sent_repr; embedding] --> d_model
+        self.merge = nn.Linear(2 * d_model, d_model)
+        self.scaler = math.sqrt(self.d_model)
+
+    def forward(self, x):
+        # this gets wrapped in Sequential, which takes only one arg
+        word_ids, sent_repr = x
+        batch, max_len = word_ids.shape
+        embs = self.lut(word_ids) * self.scaler
+
+        sent_repr = sent_repr * self.scaler
+
+        embs = embs.view(batch, max_len, self.d_model)
+        assert sent_repr.shape == (batch, self.d_model)
+        sent_repr = sent_repr.view(batch, 1, self.d_model).expand_as(embs)
+        concatd = torch.cat([sent_repr, embs], dim=-1)
+        merged = self.merge(concatd)
+        return merged
 
 
 class DecoderBlock(nn.Module):
@@ -22,14 +50,14 @@ class DecoderBlock(nn.Module):
     TODO: block is a boring name; there gotta be a more creative name for this step
     """
 
-    def __init__(self, d_model, dropout=0.1, mode='add'):
+    def __init__(self, d_model, dropout=0.1, mode='add_attn'):
         super().__init__()
-        assert mode in ('add', 'cat')
+        assert mode in ('add_attn', 'cat_attn')
         self.mode = mode
-        if mode == 'add':
+        if mode == 'add_attn':
             self.w1 = nn.Linear(d_model, d_model)
             self.w2 = nn.Linear(d_model, d_model)
-        elif mode == 'cat':
+        elif mode == 'cat_attn':
             self.w = nn.Linear(d_model + d_model, d_model)
         else:
             raise Exception()
@@ -42,9 +70,9 @@ class DecoderBlock(nn.Module):
         #        sent_repr = sent_repr.unsqueeze(1).expand_as(x)
         #  and assume they are good to concat here
 
-        if self.mode == 'add':
+        if self.mode == 'add_attn':
             scores = self.w1(x) + self.w2(sent_repr)
-        elif self.mode == 'cat':
+        elif self.mode == 'cat_attn':
             scores = self.w(torch.cat([x, sent_repr], dim=-1))
         else:
             raise Exception()
@@ -94,11 +122,12 @@ class MTransformerNMT(TransformerNMT):
 
     @classmethod
     def make_model(cls, src_vocab, tgt_vocab, n_layers=6, hid_size=512, ff_size=2048, n_heads=8,
-                   dropout=0.1, tied_emb='three-way', src_attn_mode='cat', exp: Experiment = None):
+                   dropout=0.1, tied_emb='three-way', plug_mode='cat_attn', exp: Experiment = None):
         """
         Helper: Construct a model from hyper parameters."
         :return: model, args
         """
+        assert plug_mode in {'cat_attn', 'add_atn', 'cat_emb'}
 
         # get all args for reconstruction at a later phase
         _, _, _, args = inspect.getargvalues(inspect.currentframe())
@@ -107,22 +136,27 @@ class MTransformerNMT(TransformerNMT):
         # In case you are wondering, why I didnt use **kwargs here:
         #   these args are read from conf file where user can introduce errors, so the parameter
         #   validation and default value assignment is implicitly done by function call for us :)
-
+        log.info(f"making mtfmnmt model: {args}")
         c = copy.deepcopy
         attn = MultiHeadedAttention(n_heads, hid_size)
         ff = PositionwiseFeedForward(hid_size, ff_size, dropout)
 
         enc_layer = EncoderLayer(hid_size, c(attn), c(ff), dropout)
         encoder = Encoder(enc_layer, n_layers)  # clones n times
-
-        dec_block = DecoderBlock(hid_size, dropout, mode=src_attn_mode)
-        dec_layer = MDecoderLayer(hid_size, c(attn), c(dec_block), c(ff), dropout)
-        decoder = MDecoder(dec_layer, n_layers)
-
         src_emb = nn.Sequential(Embeddings(hid_size, src_vocab),
                                 PositionalEncoding(hid_size, dropout))
-        tgt_emb = nn.Sequential(Embeddings(hid_size, tgt_vocab),
-                                PositionalEncoding(hid_size, dropout))
+
+        if plug_mode == 'cat_emb':
+            tgt_emb = nn.Sequential(MEmbeddings(hid_size, tgt_vocab),
+                                    PositionalEncoding(hid_size, dropout))
+            decoder = c(encoder)  # decoder is same as encoder, except embeddings have concat
+        else:
+            dec_block = DecoderBlock(hid_size, dropout, mode=plug_mode)
+            dec_layer = MDecoderLayer(hid_size, c(attn), c(dec_block), c(ff), dropout)
+            decoder = MDecoder(dec_layer, n_layers)
+            tgt_emb = nn.Sequential(Embeddings(hid_size, tgt_vocab),
+                                    PositionalEncoding(hid_size, dropout))
+
         generator = Generator(hid_size, tgt_vocab)
 
         model = cls(encoder, decoder, src_emb, tgt_emb, generator)
@@ -155,9 +189,16 @@ class MTransformerNMT(TransformerNMT):
 
     def decode(self, sent_repr, tgt, tgt_mask, **extra):
         assert not extra  # no extra stuff;  added to keep the lint happy
-        embs = self.tgt_embed(tgt)
-        sent_repr = sent_repr.unsqueeze(1).expand_as(embs)
-        return self.decoder(embs, tgt_mask, sent_repr)
+
+        if isinstance(self.decoder, Encoder):
+            # Sentence representation gets concatenated to embeddings
+            embs = self.tgt_embed((tgt, sent_repr))
+            return self.decoder(embs, tgt_mask)
+        else:
+            # sentence representation goes to decoder layer
+            embs = self.tgt_embed(tgt)
+            sent_repr = sent_repr.unsqueeze(1).expand_as(embs)
+            return self.decoder(embs, tgt_mask, sent_repr)
 
 
 class MTransformerTrainer(TransformerTrainer):
