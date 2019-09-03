46954d51debe388ed89f1d052689f8bd221818fd
Ljferrer <lukasjferrer@gmail.com>
2019-09-03 09:49:10 -0700

    Cleanup formatting

--- rtg/decode.py
+++ rtg/decode.py
@@ -20,8 +20,10 @@ def parse_args():
                         help='Output File path. default is STDOUT')
     parser.add_argument("-sc", '--skip-check', action='store_true',
                         help='Skip Checking whether the experiment dir is prepared and trained')
-    parser.add_argument("-b", '--batch-size', type=int, help='batch size for 1 beam. effective_batch = batch_size/beam_size')
-    parser.add_argument("-msl", '--max-src-len', type=int, help='max source len; longer seqs will be truncated')    
+    parser.add_argument("-b", '--batch-size', type=int,
+                        help='batch size for 1 beam. effective_batch = batch_size/beam_size')
+    parser.add_argument("-msl", '--max-src-len', type=int,
+                        help='max source len; longer seqs will be truncated')
     args = vars(parser.parse_args())
     return args
 
@@ -32,7 +34,7 @@ def validate_args(cli_args, conf_args, exp: Experiment):
             f'Experiment dir {exp.work_dir} is not ready to train. Please run "prep" sub task'
         assert exp.has_trained(), \
             f'Experiment dir {exp.work_dir} is not ready to decode.' \
-                f' Please run "train" sub task or --skip-check to ignore this'
+            f' Please run "train" sub task or --skip-check to ignore this'
     assert len(cli_args['input']) == len(cli_args['output'])
     if cli_args.get('batch_size'):
         batch_size = cli_args['batch_size'] / conf_args.get('beam_size', 1)
@@ -55,8 +57,8 @@ def main():
         log.info(f"Decode :: {inp} -> {out}")
         try:
             return decoder.decode_file(inp, out, **dec_args)
-        except:
-            log.exception(f"Decode failed for {inp}")
+        except Exception as e:
+            log.exception(f"Decode failed for {inp} \n\n{e}")
 
 
 if __name__ == '__main__':
