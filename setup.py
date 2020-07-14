ee3b8c499510d431f7f4d9dfc3c4a93fcd022d6b
Thamme Gowda <tg@isi.edu>
2020-07-13 22:59:16 -0700

    Add rtg-serve CLI to setup.py

--- setup.py
+++ setup.py
@@ -42,6 +42,9 @@ setuptools.setup(
         'nlcodec >= 0.2.0',
         'torch == 1.4'
     ],
+    extra_requires={
+        'serve': ['flask >= 1.1.2'],
+    },
     python_requires='>=3.7',
     entry_points={
         'console_scripts': [
@@ -52,6 +55,7 @@ setuptools.setup(
             'rtg-prep=rtg.prep:main',
             'rtg-train=rtg.train:main',
             'rtg-fork=rtg.fork:main',
+            'rtg-serve=rtg.serve:main',
             'rtg-syscomb=rtg.syscomb.__main__:main',
         ],
     }
