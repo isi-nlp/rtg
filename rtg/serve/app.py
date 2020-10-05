77ade7f4de8d9963ffe669821c0ea1f40cd92797
Thamme Gowda <tg@isi.edu>
2020-10-05 01:50:05 -0700

    RTG web UI: tokenizer and detokr are part of server

--- rtg/serve/app.py
+++ rtg/serve/app.py
@@ -7,13 +7,59 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from flask import Flask, request, jsonify, render_template, url_for, send_from_directory
 import torch
 import os
+from html import unescape
+from sacremoses import MosesTokenizer, MosesDetokenizer, MosesPunctNormalizer, MosesTruecaser
 
-from rtg import TranslationExperiment as Experiment, log
+from rtg import TranslationExperiment as Experiment
 from rtg.module.decoder import Decoder
+
+
+class RtgIO:
+
+    def __init__(self, exp):
+        self.exp = exp
+        self.tokr = MosesTokenizer()
+        self.detokr = MosesDetokenizer()
+        self.punct_normr = MosesPunctNormalizer()
+        #self.true_caser = MosesTruecaser()
+
+        self.punct_normalize = True
+        self.tokenize = True
+        self.html_unesc = True
+        self.drop_unks = True
+        #self.truecase = True
+        self.detokenize = True
+
+    def pre_process(self, text):
+        # Any pre-processing on input
+        if self.html_unesc:
+            text = unescape(text)
+        if self.punct_normalize:
+            text = self.punct_normr.normalize(text)
+        if self.tokenize:
+            text = self.tokr.tokenize(text, escape=False, return_str=True,
+                                      aggressive_dash_splits=True)
+            # protected_patterns=self.tokr.WEB_PROTECTED_PATTERNS
+        return text
+
+    def post_process(self, tokens):
+        # Any post-processing on output
+        assert isinstance(tokens, list)
+        if self.detokenize:
+            text = self.detokr.detokenize(tokens=tokens, return_str=True, unescape=True)
+        else:
+            text = " ".join(tokens)
+        if self.drop_unks:
+            text = text.replace("<unk>", "")
+        #if self.truecase:
+        #    text = self.true_caser.truecase(text, return_str=True)
+        return text
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+exp = None
+
 
 @app.route('/')
 def index():
@@ -24,7 +70,12 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static', 'img'), 'favicon.ico')
 
 
-def attach_translate_route(app, decoder, dec_args):
+def attach_translate_route(cli_args):
+    global exp
+    exp = Experiment(cli_args.pop("exp_dir"), read_only=True)
+    dec_args = exp.config.get("decoder") or exp.config["tester"].get("decoder", {})
+    decoder = Decoder.new(exp, ensemble=dec_args.pop("ensemble", 1))
+    dataprep = RtgIO(exp=exp)
 
     @app.route("/translate", methods=["POST", "GET"])
     def translate():
@@ -36,33 +87,18 @@ def attach_translate_route(app, decoder, dec_args):
             sources = (request.json or {}).get('source', None)
             if isinstance(sources, str):
                 sources = [sources]
-
         if not sources:
             return "Please submit parameter 'source'", 400
-
+        sources = [dataprep.pre_process(sent) for sent in sources]
         translations = []
         for source in sources:
             translated = decoder.decode_sentence(source, **dec_args)[0][1]
+            translated = dataprep.post_process(translated.split())
             translations.append(translated)
+
         res = dict(source=sources, translation=translations)
         return jsonify(res)
 
-
-def validate_args(cli_args, conf_args, exp: Experiment):
-    if not cli_args.pop("skip_check"):  # if --skip-check is not requested
-        assert exp.has_prepared(), (f'Experiment dir {exp.work_dir} is not ready to train.'
-                                    f' Please run "prep" sub task')
-        assert exp.has_trained(), (f"Experiment dir {exp.work_dir} is not ready to decode."
-                                   f" Please run 'train' sub task or --skip-check to ignore this")
-
-def prepare_decoder(cli_args):
-    # No grads required for decode
-    torch.set_grad_enabled(False)
-    exp = Experiment(cli_args.pop("exp_dir"), read_only=True)
-    dec_args = exp.config.get("decoder") or exp.config["tester"].get("decoder", {})
-    validate_args(cli_args, dec_args, exp)
-    decoder = Decoder.new(exp, ensemble=dec_args.pop("ensemble", 1))
-    return decoder, dec_args
 
 def parse_args():
     parser = ArgumentParser(
@@ -71,9 +107,7 @@ def parse_args():
         formatter_class=ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument("exp_dir", help="Experiment directory", type=str)
-    parser.add_argument("-sc", "--skip-check", action="store_true",
-        help="Skip Checking whether the experiment dir is prepared and trained")
-    parser.add_argument("--debug", action="store_true", help="Run Flask server in debug mode")
+    parser.add_argument("-d", "--debug", action="store_true", help="Run Flask server in debug mode")
     parser.add_argument("-p", "--port", type=int, help="port to run server on", default=6060)
     parser.add_argument("-ho", "--host", help="Host address to bind.", default='0.0.0.0')
     parser.add_argument("-msl", "--max-src-len", type=int,
@@ -84,14 +118,16 @@ def parse_args():
 
 def main():
     cli_args = parse_args()
-    decoder, dec_args = prepare_decoder(cli_args)
+    torch.set_grad_enabled(False)
+
     #CORS(app)  # TODO: insecure
     if cli_args.pop('debug'):
         app.debug = True
-    attach_translate_route(app, decoder, dec_args)
+    attach_translate_route(cli_args)
     app.run(port=cli_args["port"], host=cli_args["host"])
 
     # A very useful tutorial is found at:
     # https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
+
 if __name__ == "__main__":
     main()
