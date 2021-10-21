4adfaf4c2474bb7ee676fcc721f319d1ea616ce6
Thamme Gowda <tgowdan@gmail.com>
2021-10-21 14:52:18 -0700

    update docs

--- rtg/schema.py
+++ rtg/schema.py
@@ -10,7 +10,7 @@ from rtg.registry import CRITERION, OPTIMIZER
 def config_checks(config):
     # these are required for training models
     if 'optim' in config or OPTIMIZER not in config or CRITERION not in config:
-        help_url = 'https://github.com/isi-nlp/rtg-in/issues/260'
+        help_url = 'https://isi-nlp.github.io/rtg/v0.6.0/#migrate-to-0_6'
         log.warning("Kindly migrate the 'optim' config block to new and improved schema")
         log.info(f"For migration info visit {help_url}")
         raise ValueError(f'Config migration to new version is required; see {help_url};\n '
