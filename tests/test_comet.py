81c2cfb8c5b71118d3c01dd6a0eb7e1db96f6e9b
Thamme Gowda <thammegowda@microsoft.com>
2023-05-24 22:11:02 +0000

    Simply comet testcase

--- tests/test_comet.py
+++ tests/test_comet.py
@@ -1,18 +1,28 @@
-from rtg.comet import HfTransformerExperiment as Experiment
-from pathlib import Path
-from rtg import Pipeline
+import tempfile
 import shutil
 
+from rtg import Pipeline, load_conf, MODELS
 
 def test_comet_classifier():
-    exp = Experiment('tests/experiments/comet-cls', read_only=True)
-    exp.config['model_args'].update(dict(freeze_encoder=True))
-    exp.config['trainer'].update(dict(steps=50, check_point=25, batch_size=[4000, 4]))
-    # use smaller test set as validation -- for fast validations
-    exp.config['prep'].update(dict(valid_src="${data}.test.texts", valid_tgt="${data}.test.label"))
+    
+    
+    tmp_dir = tempfile.mkdtemp()
+    try:
+        config = load_conf('tests/experiments/comet-classifier.conf.yml')
+        model_type = config['model_type']
+        exp = MODELS[model_type].Experiment(tmp_dir, config=config, read_only=False)
+        
+        exp.config['model_args'].update(dict(freeze_encoder=True))
+        exp.config['trainer'].update(dict(steps=50, check_point=25, batch_size=[4000, 4]))
+        # use smaller test set as validation -- for fast validations
+        if exp.model_dir.exists():
+            shutil.rmtree(exp.model_dir)
+            exp.model_dir.mkdir()
 
-    if exp.model_dir.exists():
-        shutil.rmtree(exp.model_dir)
-
-    pipe = Pipeline(exp)
-    pipe.run(run_tests=False)
+        pipe = Pipeline(exp)
+        pipe.run(run_tests=False)
+        assert len(list(exp.model_dir.glob('*.pkl'))) > 0
+    finally:
+        print(f"Cleaning up {tmp_dir}")
+        shutil.rmtree(tmp_dir, ignore_errors=True)
+    
